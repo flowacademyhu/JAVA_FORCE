1b1564ddbe1133263329e9df01fac4e4ec8fc477
Tamas Vajgely <vajgi90@gmail.com>
2019-12-05 16:57:51 +0100

    Object maker

--- src/hu/flowacademy/StarWars.java
+++ src/hu/flowacademy/StarWars.java
@@ -4,23 +4,46 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.io.IOException;
+import java.util.List;
 
 public class StarWars {
-    private ArrayList<String> listOfPlayers = new ArrayList<>();
+    public static ArrayList<ForceSensitive> listOfPlayers = new ArrayList<>();
 
     public StarWars() throws IOException {
     }
 
-
-    public void actors() throws IOException {
+    public static void actors() throws IOException {
 
         BufferedReader bufReader = new BufferedReader(new FileReader("./players.txt"));
         String line = bufReader.readLine();
         while (line != null) {
-            listOfPlayers.add(line);
+            listOfPlayers.add(objectMaker(line));
             line = bufReader.readLine();
         }
         bufReader.close();
-
     }
+
+    private static ForceSensitive objectMaker(String line) {
+        String[] arr = line.split(" ");
+        if (arr[0].equals("anakin")) {
+            AnakinSkywalker anakin = new AnakinSkywalker();
+            wraithMaker(anakin, Integer.parseInt(arr[1]));
+            return anakin;
+        } else {
+            Emperor empy = new Emperor();
+            wraithMaker(empy, Integer.parseInt(arr[1]));
+            return empy;
+        }
+    }
+
+    private static void wraithMaker(ForceSensitive person, int num) {
+        if (!(person instanceof Sith)) {
+            return;
+        }
+        for (int i = 0; i < num; i++) {
+            ((Sith) person).letOutYourWraith();
+        }
+    }
+
+
 }
