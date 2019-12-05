b55c211562845c09b0017a8388ebc29a5f467d56
Istvan Dumitras <d.steven@protonmail.ch>
2019-12-05 15:58:53 +0100

    Getter-setter

--- src/hu/flowacademy/Emperor.java
+++ src/hu/flowacademy/Emperor.java
@@ -1,7 +1,29 @@
 package hu.flowacademy;
 
 public class Emperor  implements  ForceSensitive, Sith{
-    
+   private double evilMeter;
+   private double emperorPower;
+
+
+    public Emperor(){
+    evilMeter = 100;
+    }
+
+    public double getEvilMeter() {
+        return evilMeter;
+    }
+
+    public void setEvilMeter(double evilMeter) {
+        this.evilMeter = evilMeter;
+    }
+
+    public double getEmperorPower() {
+        return emperorPower;
+    }
+
+    public void setEmperorPower(double emperorPower) {
+        this.emperorPower = emperorPower;
+    }
 
     @Override
     public boolean canDefeat(ForceSensitive forceSensitive) {
@@ -16,5 +38,20 @@ public class Emperor  implements  ForceSensitive, Sith{
     @Override
     public void letOutYourWraith() {
 
+    }
+
+    public void setEmperorPower(){
+        emperorPower = evilMeter*2;
+    }
+
+
+
+
+
+    @Override
+    public String toString() {
+        return "Emperor{" +
+                "evilMeter=" + evilMeter +
+                '}';
     }
 }
